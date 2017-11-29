38fb2fa7c9f1c3595d9f9a1ab58cd0d0fe1d074d
GLOBAL\i076641 <jingjing.wang01@sap.com>
2017-11-29 18:52:52 +0800

    form group

--- src/form/form.control.provider.js
+++ src/form/form.control.provider.js
@@ -2,10 +2,12 @@
 
 angular.module('huoyun.widget').provider("FormControlProvider", [function() {
 
-  var controls = {};
+  const controls = {};
+  const widgetNames = {};
 
-  this.registerControl = function(type, control) {
+  this.registerControl = function(type, control, name) {
     controls[type] = control;
+    widgetNames[type] = name;
     return this;
   };
 
@@ -13,16 +15,26 @@ angular.module('huoyun.widget').provider("FormControlProvider", [function() {
     return controls[type];
   };
 
+  this.getWidgetNameByType = function(type) {
+    return widgetNames[type];
+  };
+
+  this.getWidgetNameByTypeOrDefault = function(type, defaultType) {
+    return widgetNames[type] || widgetNames[defaultType];
+  };
+
   this.$get = function() {
     return this;
   };
 }]);
 
-angular.module('huoyun.widget').run(["FormControlProvider", "HuoYunWidgetsInputs", function(FormControlProvider, HuoYunWidgetsInputs) {
-  FormControlProvider.registerControl("DROPDOWN", HuoYunWidgetsInputs.Dropdown);
-  FormControlProvider.registerControl("EMAIL", HuoYunWidgetsInputs.EmailBox);
-  FormControlProvider.registerControl("STRING", HuoYunWidgetsInputs.TextBox);
-  FormControlProvider.registerControl("DATALIST", HuoYunWidgetsInputs.DataList);
-  FormControlProvider.registerControl("DATE", HuoYunWidgetsInputs.Date);
-  FormControlProvider.registerControl("TIME", HuoYunWidgetsInputs.Time);
-}]);
+angular.module('huoyun.widget').run(["FormControlProvider", "HuoYunWidgetsInputs",
+  function(FormControlProvider, HuoYunWidgetsInputs) {
+    FormControlProvider.registerControl("DROPDOWN", HuoYunWidgetsInputs.Dropdown, "widgets-dropdown");
+    FormControlProvider.registerControl("EMAIL", HuoYunWidgetsInputs.EmailBox, "widgets-email-box");
+    FormControlProvider.registerControl("STRING", HuoYunWidgetsInputs.TextBox, "widgets-text-box");
+    FormControlProvider.registerControl("DATALIST", HuoYunWidgetsInputs.DataList, "widgets-data-list");
+    FormControlProvider.registerControl("DATE", HuoYunWidgetsInputs.Date, "widgets-date-box");
+    FormControlProvider.registerControl("TIME", HuoYunWidgetsInputs.Time, "widgets-time-input-box");
+  }
+]);
--- src/form/formgroup/form-group.js
+++ src/form/formgroup/form-group.js
@@ -1,15 +1,25 @@
 'use strict';
 
-angular.module('huoyun.widget').directive('widgetsFormGroup', [
-  function() {
+angular.module('huoyun.widget').directive('widgetsFormGroup', ["FormControlProvider", "$compile",
+  function(FormControlProvider, $compile) {
     return {
       restrict: 'A',
+      replace: true,
       scope: {
-        options: "=",
-        value: "=ngModel"
+        options: "="
       },
       templateUrl: 'form/formgroup/form-group.html',
-      link: function($scope, elem, attrs) {}
+      link: function($scope, elem, attrs) {
+        $scope.renderInputControl = function() {
+          var controlType = $scope.options.getType();
+          var widgetName = FormControlProvider.getWidgetNameByTypeOrDefault(controlType, "STRING");
+          var template = `<div ${widgetName} options="options.getInputControl().getInput()"></div>`;
+          var linkFn = $compile(template);
+          linkFn($scope, function(clone) {
+            elem.find(".input-control-template").append(clone);
+          });
+        };
+      }
     }
   }
 ]);
